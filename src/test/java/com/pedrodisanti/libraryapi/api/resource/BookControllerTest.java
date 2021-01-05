0956ea9a7ceb0bbd2dbb5294f41ca55e2733ebd1
Pedro Di Santi <pedro.disanti@truckpad.com.br>
2021-01-05 19:53:38 -0300

    first commit.

---
+++ src/test/java/com/pedrodisanti/libraryapi/api/resource/BookControllerTest.java
@@ -0,0 +1,79 @@
+package com.pedrodisanti.libraryapi.api.resource;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pedrodisanti.libraryapi.api.dto.BookDTO;
+import com.pedrodisanti.libraryapi.model.entity.Book;
+import com.pedrodisanti.libraryapi.service.BookService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+public class BookControllerTest {
+
+    static String BOOK_API = "/api/books";
+
+    @Autowired
+    MockMvc mvc;
+
+    @MockBean
+    BookService service;
+
+    @Test
+    @DisplayName("Should create a book with success.")
+    public void createBookTest() throws Exception{
+        BookDTO dto = BookDTO.builder()
+                .author("Alexandre Dumas")
+                .title("O Conde de Monte Cristo")
+                .isbn("27011994")
+                .build();
+
+        Book savedBook = Book.builder()
+                .id(1)
+                .author("Alexandre Dumas")
+                .title("O Conde de Monte Cristo")
+                .isbn("27011994")
+                .build();
+
+        BDDMockito.given(service.save(Mockito.any(Book.class))).willReturn(savedBook);
+
+        String json = new ObjectMapper().writeValueAsString(dto);
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(BOOK_API)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mvc.perform(request)
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").isNotEmpty())
+                .andExpect(jsonPath("title").value(dto.getTitle()))
+                .andExpect(jsonPath("author").value(dto.getAuthor()))
+                .andExpect(jsonPath("isbn").value(dto.getIsbn()));
+    }
+
+    @Test
+    @DisplayName("Should thrown an error because book's information are invalid.")
+    public void createInvalidBookTest(){
+
+    }
+}
+
