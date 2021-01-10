85562698a99d59dc59032889c1dc752c0fa3ea61
Pedro Di Santi <pedro.disanti@truckpad.com.br>
2021-01-10 14:07:02 -0300

    Criando testes de integração para métodos save e delete do repository e criando tdd de filtro de livros com Example.

--- src/main/java/com/pedrodisanti/libraryapi/service/BookService.java
+++ src/main/java/com/pedrodisanti/libraryapi/service/BookService.java
@@ -1,6 +1,8 @@
 package com.pedrodisanti.libraryapi.service;
 
 import com.pedrodisanti.libraryapi.model.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
@@ -8,6 +10,8 @@ public interface BookService {
     Book save(Book any);
 
     Optional<Book> getById(Long id);
+
+    Page<Book> find(Book filter, Pageable pageRequest);
 
     Book update(Book book);
 
--- src/test/java/com/pedrodisanti/libraryapi/model/repository/BookRepositoryTest.java
+++ src/test/java/com/pedrodisanti/libraryapi/model/repository/BookRepositoryTest.java
@@ -10,6 +10,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
@@ -39,6 +41,61 @@ public class BookRepositoryTest {
         boolean exists = repository.existsByIsbn(isbn);
 
         assertThat(exists).isTrue();
+    }
+
+    @Test
+    @DisplayName("Should return false when a book does not exists on database with the isbn.")
+    public void returnFalseWhenIsbnExists(){
+        String isbn = "27011996";
+
+
+        boolean exists = repository.existsByIsbn(isbn);
+
+        assertThat(exists).isFalse();
+    }
+
+    @Test
+    @DisplayName("Should return a book by its id.")
+    public void findByIdTest(){
+        Book book = createNewBook();
+
+        entityManager.persist(book);
+
+        Optional<Book> foundBook = repository.findById(book.getId());
+
+        assertThat(foundBook.isPresent()).isTrue();
+    }
+
+    @Test
+    @DisplayName("Should save a book and returns its id.")
+    public void saveNewBookTest(){
+        Book book = createNewBook();
+
+        Book savedBook = repository.save(book);
+
+        assertThat(savedBook.getId()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Should delete a book.")
+    public void deleteBookTest(){
+        Book book = createNewBook();
+
+        entityManager.persist(book);
+        Book foundBook = entityManager.find(Book.class, book.getId());
+
+        repository.delete(foundBook);
+
+        Book deletedBook = entityManager.find(Book.class, book.getId());
+        assertThat(deletedBook).isNull();
+    }
+
+    private Book createNewBook() {
+        return Book.builder()
+                .title("O Conde de Monte Cristo")
+                .author("Alexandre Dumas")
+                .isbn("27011994")
+                .build();
     }
 
 
